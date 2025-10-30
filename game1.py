490eb030f135bf23405faa23d10c5910c6ad56ae
noobieboi-iq <diddy114514@gmail.com>
2025-10-30 18:47:07 -0230

    Add files via upload

--- game1.py
+++ game1.py
@@ -11,53 +11,69 @@ dy=400
 cx =random.randint(0,800)
 cy =random.randint(0,800)
 
-tokens =0
+points =0
 
 font =pg.font.SysFont('Comic Sans MS', 30)
 
+def say(position, txt):
+    text = font.render(txt, False, (0, 0, 0))
+    screen.blit(text,position)
+
 background =pg.image.load("pictures/background.png")
 
-doomguy =pg.image.load("pictures/doom.png")
+doomguy =pg.image.load("pictures/char.png")
 doomguy =pg.transform.smoothscale(doomguy,(32,32))
 
-coin =pg.image.load("pictures/coin.png")
-coin =pg.transform.smoothscale(coin,(32,32))
+token =pg.image.load("pictures/token.png")
+token =pg.transform.smoothscale(token,(32,32))
         
-screen.blit(coin,(cx,cy))
+screen.blit(token,(cx,cy))
 alive =True
 
+fps =144
+
+timer =10
+    
 run = True
 while run:
     for event in pg.event.get():
         if event.type==pg.QUIT:
             run = False
-            
+    
+    timer-=1/fps
     screen.blit(background,(0,0))
-            
-    k =pg.key.get_pressed()
-    if k[pg.K_w]:
-        dy-=1
-    if k[pg.K_s]:
-        dy+=1
-    if k[pg.K_a]:
-        dx-=1
-    if k[pg.K_d]:
-        dx+=1
+    
+    if timer<=0:
+        say((300,400),"Game Over")
+        say((300,350),f"Score: {points}")
         
-    coin_hitbox =screen.blit(coin,(cx,cy))
-    doomguy_hitbox =screen.blit(doomguy,(dx,dy))
+    else:
+            
+        k =pg.key.get_pressed()
+        if k[pg.K_w]:
+            dy-=1
+        if k[pg.K_s]:
+            dy+=1
+        if k[pg.K_a]:
+            dx-=1
+        if k[pg.K_d]:
+            dx+=1
+        
+        token_hitbox =screen.blit(token,(cx,cy))
+        doomguy_hitbox =screen.blit(doomguy,(dx,dy))
     
-    text = font.render(f"tokens: {tokens}", False, (0, 0, 0))
-    screen.blit(text,(0,0))
+        say((0,0),f"points: {points}")
     
-    if doomguy_hitbox.colliderect(coin_hitbox):
-        tokens+=1
-        alive=False
+        say((0,40),f"time left: {timer:.1f}")
+        if doomguy_hitbox.colliderect(token_hitbox):
+            points+=1
+            alive=False
+            timer=5
     
-    if not alive:
-        cx =random.randint(0,800)
-        cy =random.randint(0,800)
-        alive=True
+        if not alive:
+            cx =random.randint(0,800)
+            cy =random.randint(0,800)
+            alive=True
 
     pg.display.flip()
-    clock.tick(144)
+    clock.tick(fps)
